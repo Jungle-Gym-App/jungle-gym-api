e4456c2d4a67a1dafe17abf2c57ff9ee070da1cc
Thijs Spijker <thijs@tsnetwork.nl>
2021-05-21 13:46:19 +0200

    add: filter for category

--- src/routes/games.ts
+++ src/routes/games.ts
@@ -1,7 +1,50 @@
-import {Router, Request, Response} from 'express'
+import {Router, Request, Response, NextFunction} from 'express'
 import { games as gameData } from '#mock/games'
+import { Game, Category } from '#models/game'
+import { RequestError } from 'Responses'
 
-const games = Router()
+const games: Router = Router()
 
 games
-	.get('/games', (req: Request, res: Response) => res.json(gameData))
+	.get('/', getAllGames)
+
+export default games
+
+
+function getAllGames(req: Request, res: Response, next: NextFunction) {
+	const { category } = req.query
+
+	try {
+		const filteredCategory = filterCategory(gameData, category)
+		return res.json(filteredCategory)
+	} catch(error) {
+		return res.status(error.status).json(error.body) 
+	}
+
+}
+
+
+function filterCategory(games: Game[], category: string | string[] | unknown) :Game[] {
+	const error: RequestError = {
+		status: 400,
+		body: undefined
+	}
+
+	if(!category) return games
+	else if(Array.isArray(category) && category.every(checkSyntax)) return games.filter((game) => category.includes(game.category))
+	else if(typeof category === 'string' && checkSyntax(category)) return games.filter((game) => category === game.category)
+	else throw error
+
+	function checkSyntax(category: string) {
+		switch (category) {
+		case Category.bal:
+		case Category.loop:
+		case Category.reactie:
+		case Category.tik:
+			return true
+		default: 
+			error.body = `Received an invalid filter. Got: '${category}'`
+			return false
+		}
+	}
+}
